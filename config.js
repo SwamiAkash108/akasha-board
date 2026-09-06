ef4a36bc081c4bec0cab688abba8842859410d96
SwamiAkash108 <akashananda@bhaktimarga.org>
2026-09-06 19:17:48 +0200

    v1: add config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// Akasha Board config
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://uiobotxnxfljeoqgfwyy.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_Owowsc5HUlFGaxytfGGFkw_aCzgsWZh",
+  APP_NAME: "Akasha Board"
+};
