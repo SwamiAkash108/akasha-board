2bfaf6584b5a7f4b900f054c305190060cc7bee0
SwamiAkash108 <akashananda@bhaktimarga.org>
2026-09-07 22:20:35 +0200

    fix timezone bug: all date helpers use local time, not UTC (today line was one day off in evenings)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "akasha-v15";
+const CACHE = "akasha-v16";
 const SHELL = ["./", "index.html", "styles.css", "app.js", "db.js", "config.js", "manifest.webmanifest", "icon-192.png", "icon-512.png", "bm-logo-black.png"];
 
 self.addEventListener("install", (e) => {
