f9ca064269e1f2d245ba85917d3f8a8b3bb15914
SwamiAkash108 <akashananda@bhaktimarga.org>
2026-09-07 11:51:48 +0200

    v8: Today view (overdue/today/7-day) + recurring tasks (daily/weekly/biweekly/monthly)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "akasha-v7";
+const CACHE = "akasha-v8";
 const SHELL = ["./", "index.html", "styles.css", "app.js", "db.js", "config.js", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", (e) => {
